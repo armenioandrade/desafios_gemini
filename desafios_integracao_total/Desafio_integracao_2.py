173f39e6423ce3f83f720795379a2f093a1bb5b5
armenioandrade <armenioandrade@gmail.com>
2025-10-08 22:16:14 -0300

    sistema de orçamento simples completo.

--- desafios_integracao_total/Desafio_integracao_2.py
+++ desafios_integracao_total/Desafio_integracao_2.py
@@ -93,11 +93,85 @@ class Cliente(Pessoa):
             print('Erro no cadastro do cpf: ',t)
         else:
             self._cpf = novo_cpf.replace('.','').replace('-','')
+            print('cpf cadastrado!')
 
 class Item():
+
+    quantidade = 0    
 
     def __init__(self, nome_item, valor_item):
         self.nome_item = nome_item
         self.valor_item = valor_item
+
+    @property
+    def nome_item(self):
+        return self._nome_item
+
+    @nome_item.setter
+    def nome_item(self, novo_nome):
+        try:
+            if not isinstance(novo_nome, str):
+                raise TypeError('Tipo precisa ser texto')
+        except TypeError as t:
+            print('Erro no cadastro do item: ',t)
+        else:
+            self._nome_item = novo_nome
+            print('item cadastrado!')
+
+    @property
+    def valor_item(self):
+        return self._valor_item
     
+    @valor_item.setter
+    def valor_item(self, novo_valor):
+        try:
+            if not isinstance(novo_valor, (float, int)):
+                raise TypeError('Tipo do item precisa ser numérico')
+
+            if novo_valor <= 0:
+                raise ValueError('valor do item não pode ser menor ou igual a zero')
+        
+        except TypeError as t:
+            print('Erro no tipo do item: ', t)
+        except ValueError as v:
+            print('Erro no valor do item: ', v)
+        else:
+            self._valor_item = novo_valor
+            print('valor do item cadastrado!')
+ 
+    def adicionar_quantidade(self, quantidade):
+        self.quantidade = quantidade
+    
+    def listar_itens(self):
+        return f'QUANTIDADE - NOME DO ITEM\n{self.quantidade} | {self.nome_item}'
+
+
+
+class Orcamento():
+    def __init__(self, nome_cliente, cpf, nome_funcionario, login, senha, nome_item, valor_item):
+        self.cliente = Cliente(nome_cliente, cpf)
+        self.funcionario = Funcionario(nome_funcionario, login, senha)
+        self.item = Item(nome_item, valor_item)
+    
+    def gerar_orcamento(self):
+        try:
+            with open('orcamento.txt', 'w') as f:
+                f.write('ORCAMENTO SIMPLES\n')
+                f.write('*' * 30 + '\n')
+                f.write('DADOS DO CLIENTE' + '\n')
+                f.write(f'NOME: {self.cliente.nome}\n')
+                f.write(f'CPF: {self.cliente.cpf}\n')
+                f.write('*' * 30 + '\n')
+                f.write('DADOS DO FUNCIONARIO' + '\n')
+                f.write(f'NOME: {self.funcionario.nome}\n')
+                f.write(f'LOGIN: {self.funcionario.login}\n')
+                f.write('*' * 30 + '\n')
+                f.write('ITENS DO ORCAMENTO\n')
+                f.write(self.item.listar_itens())
+        except Exception as e:
+            print('Erro a tratar ', e)
+        
+orc1 = Orcamento('amaro','12345','armenio','armenio.andrade', 1234, 'iphone', 1000)
+orc1.item.adicionar_quantidade(10)
+orc1.gerar_orcamento()
                 
